0507332b97cbec14e9c873b5b76da7a6bff7e7dd
cata-zaro <70031076+cata-zaro@users.noreply.github.com>
2020-09-10 21:48:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
+
+let sketch;
+
 function setup() {
-  createCanvas(800, 400);
+  sketch = createCanvas(800, 400);
+  sketch.parent("p5");
+  frameRate(4);
 }
 
 function draw() {
-  point(random(width), random(height));
-  
+  background(255);
+  for(let i = 0; i < 20; i++){
+  // define color aleatorio 
+  stroke(random(255), random(255), random(255));
+  // defino un grosor aleatorio
+  strokeWeight(random(2, 20));
+  // dubujo un punto aleatorio
+  point(random(10, width-10), random(10, height - 10));
+  }
 }
+
